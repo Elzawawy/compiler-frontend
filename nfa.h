7a307ec28a106c4120b63824101d52ab10ba484b
abdallahmaradny <abdallahmaradny@gmail.com>
2020-03-25 02:34:54 +0200

    Structuring and declaring funcitons

--- nfa.h
+++ nfa.h
@@ -1,4 +1,3 @@
-//
 // Created by omar_swidan on 23/03/20.
 //
 
@@ -7,14 +6,27 @@
 
 using namespace std;
 
+#include <iostream>
+#include<stack>
 #include <string>
 #include <unordered_set>
 #include "nfa_state.h"
-
 class NFA {
 public:
+
     NFA();
-    NFAState regex_to_nfa(string regex, unordered_set<string> input_table);
+    NFAState regex_to_nfa();
+
+private:
+    NFAState concat(NFAState first_nfa_state,NFAState second_nfa_state);
+    NFAState or_combiner(vector<NFAState> selections, int no_of_selections);
+    NFAState kleene(NFAState nfa_state);
+    string infix_to_postfix(string regex);
+    string pre_process_regex(string regex);
+    int precedence_decision(string operator_symbol);
+    string char_to_string_converter(char character);
+    bool is_operator(char character);
+    bool is_operand(char character);
 
 };
 
