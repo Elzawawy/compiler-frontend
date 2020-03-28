e6068c3aa3d1625126cf96a5270f8566de316cd1
omar_swidan <oswidan97@gmail.com>
2020-03-28 20:03:04 +0200

    [LEXGEN-5] Add lexical analyzer driver class h file definitions

--- lexical_analyzer_driver.h
+++ lexical_analyzer_driver.h
@@ -15,7 +15,21 @@
 using std::string;
 
 class LexicalAnalyzerDriver {
- 
+ private:
+  bool input_over_;
+  const string input_file_name_;
+  DFAState *root_state_;
+  ifstream input_file_;
+  const int buffer_size_ = 4096;
+  vector<char> buffer_1_, buffer_2_;
+  vector<char>::iterator forward_, lexeme_begin_;
+  stack<pair<char, DFAState*>> states_;
+  streamsize current_buffer_size_;
+ public:
+  LexicalAnalyzerDriver(DFAState *root_state_, const string &input_file_name_);
+  Token *GetNextToken();
+  bool IsInputOver();
+  virtual ~LexicalAnalyzerDriver();
 };
 
 #endif //LEXGEN_LEXICAL_ANALYZER_DRIVER_H
