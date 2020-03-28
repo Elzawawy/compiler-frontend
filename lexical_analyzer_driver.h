1ba0d3e7fcbe28d20be984881f321d4a53d02d6d
omar_swidan <oswidan97@gmail.com>
2020-03-28 20:02:43 +0200

    [LEXGEN-5] Add lexical analyzer driver class h file

---
+++ lexical_analyzer_driver.h
@@ -0,0 +1,21 @@
+//
+// Created by omar_swidan on 27/03/20.
+//
+
+#ifndef LEXGEN_LEXICAL_ANALYZER_DRIVER_H
+#define LEXGEN_LEXICAL_ANALYZER_DRIVER_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stack>
+#include "dfa_state.h"
+#include "token.h"
+
+using std::string;
+
+class LexicalAnalyzerDriver {
+ 
+};
+
+#endif //LEXGEN_LEXICAL_ANALYZER_DRIVER_H
