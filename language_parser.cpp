df66f40c63f44d8d94d1e131243ddaa5c9ca4d89
Elzawawy <amr22.elzawawy@ymail.com>
2020-03-28 07:28:38 +0200

    Handle parsing and nesting rules in definitions and expressions.

--- language_parser.cpp
+++ language_parser.cpp
@@ -5,7 +5,16 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <algorithm>
 #include "language_parser.h"
+
+#define KEYWORD_INDICATOR '{'
+#define KEYWORD "Keyword"
+#define PUNCTUATION_INDICATOR '['
+#define PUNCTUATION "Punctuation"
+#define REGULAR_EXP_SPLITTER ":="
+#define REGULAR_EXP_INDICATOR ':'
+typedef unsigned long ul;
 
 const std::vector<RegularExpression>& LanguageParser::getExpressions() const
 {
@@ -25,49 +34,65 @@ void LanguageParser::parseFile(std::string rules_file_path)
 
         file.close();
     }
+
+//    for(auto& expression: expressions_)
+//        std::cout<<expression.getName()<<" "<<expression.getValue()<<std::endl;
+//
+//    for(auto& expression: definitions_)
+//        std::cout<<expression.first<<" "<<expression.second<<std::endl;
 }
 
-std::string& trimBothEnds(std::string& str, const std::string& chars = "\t\n\v\f\r ")
+void trimBothEnds(std::string& str, const std::string& chars = "\t\n\v\f\r ")
 {
     str.erase(str.find_last_not_of(chars)+1);
     str.erase(0, str.find_first_not_of(chars));
 }
 
-std::string& stripFirstAndLastChars(std::string& str)
+void stripFirstAndLastChars(std::string& str)
 {
     str.erase(0, 1);
     str.erase(str.length()-1, str.length()-1);
 }
 
-std::vector<std::string> splitOnDelimiter(const std::string& s, char delimiter)
+std::vector<std::string> splitOnDelimiter(const std::string& str, char delimiter)
 {
     std::vector<std::string> tokens;
     std::string token;
-    std::istringstream tokenStream(s);
+    std::istringstream tokenStream(str);
     while (std::getline(tokenStream, token, delimiter))
         tokens.push_back(token);
-
     return tokens;
 }
 
 void LanguageParser::parseRule(std::string rule)
 {
-    switch (rule[0]) {
-    case '[': {
+    if (rule[0]==KEYWORD_INDICATOR or rule[0]==PUNCTUATION_INDICATOR) {
+        std::string regex_name = (rule[0]==KEYWORD_INDICATOR) ? KEYWORD : PUNCTUATION;
         stripFirstAndLastChars(rule);
         trimBothEnds(rule);
         std::vector<std::string> tokens = splitOnDelimiter(rule, ' ');
-        break;
+        for (auto& regex_value: tokens)
+            expressions_.emplace_back(regex_name, regex_value);
     }
-    case '{': {
-        stripFirstAndLastChars(rule);
-        trimBothEnds(rule);
-        std::vector<std::string> tokens = splitOnDelimiter(rule, ' ');
-        break;
-    }
-    default:
-        //handleRegularExpressionAndDefinition()
-        break;
+    else {
+        rule.erase(std::remove(rule.begin(), rule.end(), ' '), rule.end());
+        ul found = rule.find_first_of(REGULAR_EXP_SPLITTER);
+        std::reverse(definitions_.begin(), definitions_.end());
+        for (auto& definition: definitions_) {
+            ul definition_pos = rule.find(definition.first, found+1);
+            while (definition_pos!=std::string::npos) {
+                rule.replace(definition_pos, definition.first.length(), "("+definition.second+")");
+                std::cout << rule << std::endl;
+                definition_pos = rule.find(definition.first, found+1);
+            }
+        }
+
+        std::string regex_name = rule.substr(0, found);
+        std::string regex_value = rule.substr(found+1);
+        if (rule[found]==REGULAR_EXP_INDICATOR)
+            expressions_.emplace_back(regex_name, regex_value);
+        else
+            definitions_.emplace_back(regex_name, regex_value);
     }
 
 }
