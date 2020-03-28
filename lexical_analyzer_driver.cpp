e99ba3f62b0db192e5a1849fe645edf4d7c2f7b5
omar_swidan <oswidan97@gmail.com>
2020-03-28 20:04:23 +0200

    [LEXGEN-5] Add lexical analyzer driver GetToken reading buffers logic

--- lexical_analyzer_driver.cpp
+++ lexical_analyzer_driver.cpp
@@ -22,3 +22,35 @@ LexicalAnalyzerDriver::LexicalAnalyzerDriver( DFAState *root_state_, const strin
   this->states_.push(make_pair('L',this->root_state_));
 }
 
+//The caller has to check if no more input is present by calling IsInputOver method
+//The caller is the parser.
+Token *LexicalAnalyzerDriver::GetNextToken() {
+  //start with setting lexeme_begin with forward in order to satrt a new lexeme
+  this->lexeme_begin_ = this->forward_;
+
+  //Reading the buffers. Can be done more neatly. Refactor in the end.
+  if (this->forward_ == this->buffer_1_.end()) {
+    this->input_file_.read(this->buffer_2_.data(), this->buffer_size_);
+    this->forward_ = this->buffer_2_.begin();
+    this->current_buffer_size_ = ((this->input_file_) ? this->buffer_size_ : this->input_file_.gcount());
+    this->input_over_ = !this->input_file_.good();
+  } else if (this->forward_ == this->buffer_2_.end()) {
+    this->input_file_.read(this->buffer_1_.data(), this->buffer_size_);
+    this->forward_ = this->buffer_2_.begin();
+    this->current_buffer_size_ = ((this->input_file_) ? this->buffer_size_ : this->input_file_.gcount());
+    this->input_over_ = !this->input_file_.good();
+  }
+  if (this->input_over_) return nullptr;
+  DFAState* neighbour = this->states_.top().second->GetNeighbour(*this->forward_);
+  if (neighbour->isAccepting_state()) {
+    return
+  }
+
+
+
+
+  return nullptr;
+}
+LexicalAnalyzerDriver::~LexicalAnalyzerDriver() {
+  this->input_file_.close();
+}
