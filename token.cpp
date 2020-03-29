1bbdbeb82cce608a063d6bfe3e21a0d1640c4f04
omar_swidan <oswidan97@gmail.com>
2020-03-29 16:22:45 +0200

    [LEXGEN-5] Add the token.cpp for the iplementation of some functions

--- token.cpp
+++ token.cpp
@@ -3,3 +3,10 @@
 //
 
 #include "token.h"
+Token::Token(const string lexeme_, const string token_name_) : lexeme_(lexeme_), token_name_(token_name_) {}
+const string &Token::GetLexeme_() const {
+  return lexeme_;
+}
+const string &Token::GetTokenName() const {
+  return token_name_;
+}
