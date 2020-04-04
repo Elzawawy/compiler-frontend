85fbbb5cf7fa3d032f0384099258ec44b2452b8c
omar_swidan <oswidan97@gmail.com>
2020-04-04 22:29:15 +0200

    [LEXGEN-5] Add an h file for the min dfa

---
+++ min_dfa.h
@@ -0,0 +1,15 @@
+//
+// Created by omar_swidan on 03/04/20.
+//
+
+#ifndef LEXGEN_MIN_DFA_H
+#define LEXGEN_MIN_DFA_H
+
+#include "dfa_state.h"
+
+unordered_set<DFAState *>  partitioning(DFAState *** transitionTable , unordered_set<DFAState *> listofstates);
+
+DFAState ***   constructTransitiontable( DFAState* root , unordered_set<DFAState *> listofstates);
+
+
+#endif //LEXGEN_MIN_DFA_H
