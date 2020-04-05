18a7856127e61664a7cbd194c5276caa86c173b5
omar_swidan <oswidan97@gmail.com>
2020-04-05 21:46:56 +0200

    [LEXGEN-5] Modify tests

--- tests/whole_pipeline_tests.cpp
+++ tests/whole_pipeline_tests.cpp
@@ -7,22 +7,77 @@
 #include "../lexical_analyzer_driver.h"
 #include "../token.h"
 
-TEST_CASE("PDF's inputs"){
+TEST_CASE("PDF's inputs") {
     auto language_parser = new LanguageParser();
     language_parser->parseFile("../test_cases/a_lexical_rules.txt");
     NFA x = NFA();
     NFAState *nfa_start_state = x.regex_to_nfa(language_parser->getInput_table(), language_parser->getExpressions());
     DFA dfa_gen;
-    DFAState *dfa_start_state = dfa_gen.GenerateDFA(*nfa_start_state, language_parser->getInput_table());
+    DFAState *dfa_start_state = dfa_gen.GenerateDFA(*nfa_start_state, x.get_input_table());
 
 
 //    constructTransitiontable(dfa_start_state, dfa_gen.getMarked_dfa_states_()), dfa_gen.getMarked_dfa_states_();
 //    unordered_set<DFAState *>  newDFA = partitioning(constructTransitiontable(dfa_start_state, dfa_gen.getMarked_dfa_states_()), dfa_gen.getMarked_dfa_states_());
+//
+//    constructTransitiontable(*(newDFA.begin()), newDFA);
 
-//        constructTransitiontable(*(newDFA.begin()), newDFA);
-
-    LexicalAnalyzerDriver lexicalAnalyzerDriver(dfa_start_state, "../test_cases/a_test_program1.txt");
-    Token *token1 = lexicalAnalyzerDriver.GetNextToken();
-
+//    LexicalAnalyzerDriver lexicalAnalyzerDriver(dfa_start_state, "../test_cases/a_test_program1.txt");
+//
+//   Token *token = (lexicalAnalyzerDriver.GetNextToken());
+//
+//    REQUIRE(token->GetLexeme_() == "int");
+//    delete token;
+//    token = (lexicalAnalyzerDriver.GetNextToken());
+//    REQUIRE(token->GetLexeme_() == "sum");
+//    delete token;
+//    token = (lexicalAnalyzerDriver.GetNextToken());
+//    REQUIRE(token->GetLexeme_() == ",");
+//    delete token;
+//    token = (lexicalAnalyzerDriver.GetNextToken());
+//    REQUIRE(token->GetLexeme_() == "count");
+//    delete token;
+//    token = (lexicalAnalyzerDriver.GetNextToken());
+//    REQUIRE(token->GetLexeme_() == ",");
+//    delete token;
+//    token = (lexicalAnalyzerDriver.GetNextToken());
+//    REQUIRE(token->GetLexeme_() == "pass");
+//    delete token;
+//    token = (lexicalAnalyzerDriver.GetNextToken());
+//    REQUIRE(token->GetLexeme_() == ",");
+//    delete token;
+//    token = (lexicalAnalyzerDriver.GetNextToken());
+//    REQUIRE(token->GetLexeme_() == "mnt");
+//    delete token;
+//    token = (lexicalAnalyzerDriver.GetNextToken());
+//    REQUIRE(token->GetLexeme_() == ";");
+//    delete token;
+//    token = (lexicalAnalyzerDriver.GetNextToken());
+//    REQUIRE(token->GetLexeme_() == "while");
+//    delete token;
+//    token = (lexicalAnalyzerDriver.GetNextToken());
+//    REQUIRE(token->GetLexeme_() == "(");
+//    delete token;
+//    token = (lexicalAnalyzerDriver.GetNextToken());
+//    REQUIRE(token->GetLexeme_() == "pass");
+//    delete token;
+//    token = (lexicalAnalyzerDriver.GetNextToken());
+//    REQUIRE(token->GetLexeme_() == "!=");
+//    delete token;
+//    token = (lexicalAnalyzerDriver.GetNextToken());
+//    REQUIRE(token->GetLexeme_() == "10");
+////    delete token;
+//    token = (lexicalAnalyzerDriver.GetNextToken());
+//    REQUIRE(token->GetLexeme_() == ")");
+//    delete token;
+//    REQUIRE((lexicalAnalyzerDriver.GetNextToken())->GetLexeme_() == "{");
+//    REQUIRE((lexicalAnalyzerDriver.GetNextToken())->GetLexeme_() == "pass");
+//    REQUIRE((lexicalAnalyzerDriver.GetNextToken())->GetLexeme_() == "=");
+//    REQUIRE((lexicalAnalyzerDriver.GetNextToken())->GetLexeme_() == "pass");
+//    REQUIRE((lexicalAnalyzerDriver.GetNextToken())->GetLexeme_() == "+");
+//    REQUIRE((lexicalAnalyzerDriver.GetNextToken())->GetLexeme_() == "1");
+//    REQUIRE((lexicalAnalyzerDriver.GetNextToken())->GetLexeme_() == ";");
+//    REQUIRE((lexicalAnalyzerDriver.GetNextToken())->GetLexeme_() == "}");
+    delete dfa_start_state;
+    delete nfa_start_state;
 
 }
