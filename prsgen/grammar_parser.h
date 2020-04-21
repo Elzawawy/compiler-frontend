ae1ada0b34fc00f9d08620292c4d0a6c567dd393
Elzawawy <amr22.elzawawy@ymail.com>
2020-04-21 16:12:58 +0200

    [PRSGEN-1] Add Non Terminal Class

--- prsgen/grammar_parser.h
+++ prsgen/grammar_parser.h
@@ -5,8 +5,11 @@
 #ifndef PRSGEN_GRAMMAR_PARSER_H
 #define PRSGEN_GRAMMAR_PARSER_H
 
+#include <set>
 class GrammarParser {
 
+private:
+    std::set<std::string> terminals;
 };
 
 #endif //PRSGEN_GRAMMAR_PARSER_H
---
+++ prsgen/non_terminal.cpp
@@ -0,0 +1,45 @@
+//
+// Created by zawawy on 4/21/20.
+//
+
+#include "non_terminal.h"
+
+NonTerminal::NonTerminal(const std::string& name_)
+        :name_(name_) { }
+
+const std::string& NonTerminal::getName_() const
+{
+    return name_;
+}
+const std::set<std::string>& NonTerminal::getFirst_() const
+{
+    return first_;
+}
+void NonTerminal::setFirst_(const std::set<std::string>& first_)
+{
+    NonTerminal::first_ = first_;
+}
+const std::set<std::string>& NonTerminal::getFollow_() const
+{
+    return follow_;
+}
+void NonTerminal::setFollow_(const std::set<std::string>& follow_)
+{
+    NonTerminal::follow_ = follow_;
+}
+const std::vector<std::vector<std::string>>& NonTerminal::getProduction_rules_() const
+{
+    return production_rules_;
+}
+void NonTerminal::setProduction_rules_(const std::vector<std::vector<std::string>>& production_rules_)
+{
+    NonTerminal::production_rules_ = production_rules_;
+}
+const std::map<std::string, int>& NonTerminal::getParse_table_entry_() const
+{
+    return parse_table_entry_;
+}
+void NonTerminal::setParse_table_entry_(const std::map<std::string, int>& parse_table_entry_)
+{
+    NonTerminal::parse_table_entry_ = parse_table_entry_;
+}
