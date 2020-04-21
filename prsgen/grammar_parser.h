cc9521be2e1fb838c0cad8aa639078d21e03ecdd
Elzawawy <amr22.elzawawy@ymail.com>
2020-04-21 15:26:51 +0200

    [PRSGEN-1] Add Parsing table Generator Class

--- prsgen/grammar_parser.h
+++ prsgen/grammar_parser.h
@@ -2,11 +2,11 @@
 // Created by zawawy on 4/21/20.
 //
 
-#ifndef LEXGEN_GRAMMER_PARSER_H
-#define LEXGEN_GRAMMAR_PARSER_H
+#ifndef PRSGEN_GRAMMAR_PARSER_H
+#define PRSGEN_GRAMMAR_PARSER_H
 
 class GrammarParser {
 
 };
 
-#endif //LEXGEN_GRAMMAR_PARSER_H
+#endif //PRSGEN_GRAMMAR_PARSER_H
---
+++ prsgen/parsing_table_generator.h
@@ -0,0 +1,12 @@
+//
+// Created by zawawy on 4/21/20.
+//
+
+#ifndef PRSGEN_PARSING_TABLE_GENERATOR_H
+#define PRSGEN_PARSING_TABLE_GENERATOR_H
+
+class ParsingTableGenerator {
+
+};
+
+#endif //PRSGEN_PARSING_TABLE_GENERATOR_H
