38bc82b664bcf25ac72a2c0d37da3a83fc12d67b
Elzawawy <amr22.elzawawy@ymail.com>
2020-04-21 15:29:54 +0200

    [PRSGEN-1] Add Predictive Parser Class

---
+++ prsgen/predictive_parser.h
@@ -0,0 +1,12 @@
+//
+// Created by zawawy on 4/21/20.
+//
+
+#ifndef PRSGEN_PREDICTIVE_PARSER_H
+#define PRSGEN_PREDICTIVE_PARSER_H
+
+class PredicativeParser {
+
+};
+
+#endif //PRSGEN_PREDICTIVE_PARSER_H
