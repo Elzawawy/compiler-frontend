31ba14ff306dddd0d97a56862240da5a810d4dc4
omar_swidan <oswidan97@gmail.com>
2020-05-01 16:52:06 +0200

    [PRSGEN-5] Change the sets used to unirdered sets and add a map for the non terminals

--- prsgen/parsing_table_generator.h
+++ prsgen/parsing_table_generator.h
@@ -1,6 +1,4 @@
-//
-// Created by zawawy on 4/21/20.
-//
+#include <unordered_map>
 #include "non_terminal.h"
 
 #ifndef PRSGEN_PARSING_TABLE_GENERATOR_H
@@ -8,10 +6,11 @@
 
 class ParsingTableGenerator {
 private:
-    std::set<std::string>& terminals_;
+    std::unordered_set<std::string>& terminals_;
     std::vector<NonTerminal>& non_terminals_;
+    std::unordered_map<std::string, NonTerminal*> name_non_terminal_;
 public:
-    ParsingTableGenerator(std::set<std::string>& terminals_, std::vector<NonTerminal>& non_terminals_);
+    ParsingTableGenerator(std::unordered_set<std::string>& terminals_, std::vector<NonTerminal>& non_terminals_);
     void computeFirst();
     void computeFollow();
     void constructParsingTable();
