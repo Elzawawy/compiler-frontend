2445c36c916ddb93860b8ebae8a4cc37d0f5388b
omar_swidan <oswidan97@gmail.com>
2020-05-02 23:18:21 +0200

    [PRSGEN-5] Add test for follow set

--- prsgen/tests/first_set_tests.cpp
+++ prsgen/tests/first_set_tests.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-TEST_CASE("Creating First Sets") {
+TEST_CASE("Creating First and Follow Sets") {
     NonTerminal n1("E");
     NonTerminal n2("E`");
     NonTerminal n3("T");
@@ -16,19 +16,29 @@ TEST_CASE("Creating First Sets") {
     NonTerminal n5("F");
     n1.setProduction_rules_(vector<vector<string>>{{"T", "E`"}});
     n2.setProduction_rules_(vector<vector<string>>{{"+", "T", "E`"},
-                                                   {"\L"}});
+                                                   {"\\L"}});
     n3.setProduction_rules_(vector<vector<string>>{{"F", "T`"}});
     n4.setProduction_rules_(vector<vector<string>>{{"*", "F", "T`"},
-                                                   {"\L"}});
+                                                   {"\\L"}});
     n5.setProduction_rules_(vector<vector<string>>{{"(", "E", ")"},
                                                    {"id"}});
     unordered_set<string> terminals{"+", "*", "(", ")", "id"};
     vector<NonTerminal> non_terminals{n1, n2, n3, n4, n5};
     ParsingTableGenerator parsingTableGenerator(terminals, non_terminals);
     parsingTableGenerator.computeFirst();
+    parsingTableGenerator.computeFollow();
+
     for (auto &&non_terminal: non_terminals) {
         cout<<non_terminal.getName_()<<" first set: ";
         for(auto &&element: non_terminal.getFirst_()) {
+            cout<<element<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<"========================================================="<<endl;
+    for (auto &&non_terminal: non_terminals) {
+        cout<<non_terminal.getName_()<<" follow set: ";
+        for(auto &&element: non_terminal.getFollow_()) {
             cout<<element<<" ";
         }
         cout<<endl;
