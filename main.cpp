e06c8af52d95d61b459cdaf5f8ef950db81b6c87
Elzawawy <amr22.elzawawy@ymail.com>
2020-05-03 04:38:45 +0200

    [PRSGEN-3] Bug Fix for Eliminate Left recursion

--- main.cpp
+++ main.cpp
@@ -12,7 +12,8 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
 
 //    auto language_parser = new LanguageParser();
 //    language_parser->parseFile("../test_cases/a_lexical_rules.txt");
@@ -20,142 +21,143 @@ int main() {
 //    NFAState *nfa_start_state = x.regex_to_nfa(language_parser->getInput_table(), language_parser->getExpressions());
 //    DFA dfa_gen = DFA();
 //    DFAState *dfa_start_state = dfa_gen.GenerateDFA(*nfa_start_state, x.get_input_table());
-	GrammarParser *X = new GrammarParser();
-	
-	/*
-	NonTerminal* A = new NonTerminal("A");
+    GrammarParser* X = new GrammarParser();
 
-	std::vector<std::vector<std::string> > prodrules;
-	
-	std::vector<std::string> tmp;
-	tmp.push_back("A");
-	tmp.push_back("C");
-	prodrules.push_back(tmp);
-	
-	std::vector<std::string> tmp2;
-	tmp2.push_back("S");
-	tmp2.push_back("d");
-	
-	prodrules.push_back(tmp2);
-		 
-	std::vector<std::string> tmp3;
-	tmp3.push_back("f");
-	
-	prodrules.push_back(tmp3);
+    /*
+    NonTerminal* A = new NonTerminal("A");
 
+    std::vector<std::vector<std::string> > prodrules;
 
+    std::vector<std::string> tmp;
+    tmp.push_back("A");
+    tmp.push_back("C");
+    prodrules.push_back(tmp);
 
-	A->setProduction_rules_(prodrules);
+    std::vector<std::string> tmp2;
+    tmp2.push_back("S");
+    tmp2.push_back("d");
 
+    prodrules.push_back(tmp2);
 
-	NonTerminal* S = new NonTerminal("S");
+    std::vector<std::string> tmp3;
+    tmp3.push_back("f");
 
-	prodrules.clear();
-
-	tmp.clear();
-	tmp.push_back("A");
-	tmp.push_back("a");
-	prodrules.push_back(tmp);
-
-	 tmp2.clear();
-	tmp2.push_back("b");
-
-	prodrules.push_back(tmp2);
+    prodrules.push_back(tmp3);
 
 
 
+    A->setProduction_rules_(prodrules);
 
 
-	S->setProduction_rules_(prodrules);
+    NonTerminal* S = new NonTerminal("S");
+
+    prodrules.clear();
+
+    tmp.clear();
+    tmp.push_back("A");
+    tmp.push_back("a");
+    prodrules.push_back(tmp);
+
+     tmp2.clear();
+    tmp2.push_back("b");
+
+    prodrules.push_back(tmp2);
 
 
-	std::vector<NonTerminal *> terminals;
-	terminals.push_back(S);
-	terminals.push_back(A);
-	//cout << X->checkImmediateLeftRecursion(A);
+
+
+
+    S->setProduction_rules_(prodrules);
+
+
+    std::vector<NonTerminal *> terminals;
+    terminals.push_back(S);
+    terminals.push_back(A);
+    //cout << X->checkImmediateLeftRecursion(A);
 //	X->eliminateImmediateLeftRecursion(A);
 
 //	X->subsitution(A, S);
-	X->eliminateLeftRecursion(terminals);
-	*/
+    X->eliminateLeftRecursion(terminals);
+    */
+//
+//	NonTerminal* S = new NonTerminal("S");
+//
+//	std::vector<std::vector<std::string> > prodrules;
+//
+//	std::vector<std::string> tmp2;
+//	std::vector<std::string> tmp3;
+//	std::vector<std::string> tmp;
+//	tmp.push_back("E");
+//	prodrules.push_back(tmp);
+//	S->setProduction_rules_(prodrules);
+//
+//
+//	NonTerminal* E = new NonTerminal("E");
+//
+//	prodrules.clear();
+//
+//	tmp.clear();
+//	tmp.push_back("E");
+//	tmp.push_back("+");
+//	tmp.push_back("T");
+//	prodrules.push_back(tmp);
+//
+//	tmp2.clear();
+//	tmp2.push_back("T");
+//
+//	prodrules.push_back(tmp2);
+//
+//	E->setProduction_rules_(prodrules);
+//
+//
+//	NonTerminal* T = new NonTerminal("T");
+//
+//	prodrules.clear();
+//
+//	tmp.clear();
+//	tmp.push_back("E");
+//	tmp.push_back("-");
+//	tmp.push_back("T");
+//	prodrules.push_back(tmp);
+//
+//	tmp2.clear();
+//	tmp2.push_back("F");
+//
+//	prodrules.push_back(tmp2);
+//
+//	T->setProduction_rules_(prodrules);
+//
+//	NonTerminal* F = new NonTerminal("F");
+//
+//	prodrules.clear();
+//
+//	tmp.clear();
+//	tmp.push_back("E");
+//	tmp.push_back("*");
+//	tmp.push_back("F");
+//	prodrules.push_back(tmp);
+//
+//	tmp2.clear();
+//	tmp2.push_back("id");
+//
+//	prodrules.push_back(tmp2);
+//
+//	F->setProduction_rules_(prodrules);
+//
+//
+//	std::vector<NonTerminal > terminals;
+//	terminals.push_back(*S);
+//	terminals.push_back(*E);
+//	terminals.push_back(*T);
+//	terminals.push_back(*F);
+//
+//
+//
+//	 X->eliminateLeftRecursion(terminals);
+    GrammarParser parser;
 
-	NonTerminal* S = new NonTerminal("S");
-
-	std::vector<std::vector<std::string> > prodrules;
-
-	std::vector<std::string> tmp2;
-	std::vector<std::string> tmp3;
-	std::vector<std::string> tmp;
-	tmp.push_back("E");
-	prodrules.push_back(tmp);
-	S->setProduction_rules_(prodrules);
 
 
-	NonTerminal* E = new NonTerminal("E");
-
-	prodrules.clear();
-
-	tmp.clear();
-	tmp.push_back("E");
-	tmp.push_back("+");
-	tmp.push_back("T");
-	prodrules.push_back(tmp);
-
-	tmp2.clear();
-	tmp2.push_back("T");
-
-	prodrules.push_back(tmp2);
-
-	E->setProduction_rules_(prodrules);
-
-
-	NonTerminal* T = new NonTerminal("T");
-
-	prodrules.clear();
-
-	tmp.clear();
-	tmp.push_back("E");
-	tmp.push_back("-");
-	tmp.push_back("T");
-	prodrules.push_back(tmp);
-
-	tmp2.clear();
-	tmp2.push_back("F");
-
-	prodrules.push_back(tmp2);
-
-	T->setProduction_rules_(prodrules);
-
-	NonTerminal* F = new NonTerminal("F");
-
-	prodrules.clear();
-
-	tmp.clear();
-	tmp.push_back("E");
-	tmp.push_back("*");
-	tmp.push_back("F");
-	prodrules.push_back(tmp);
-
-	tmp2.clear();
-	tmp2.push_back("id");
-
-	prodrules.push_back(tmp2);
-
-	F->setProduction_rules_(prodrules);
-
-
-	std::vector<NonTerminal > terminals;
-	terminals.push_back(*S);
-	terminals.push_back(*E);
-	terminals.push_back(*T);
-	terminals.push_back(*F);
-
-	
-
-	 X->eliminateLeftRecursion(terminals);
-
-
-
-	//TEST CASE LINK
+    //TEST CASE LINK
     return 0;
 }
--- prsgen/grammar_parser.cpp
+++ prsgen/grammar_parser.cpp
@@ -19,215 +19,182 @@ void GrammarParser::parseFile(std::string file_name)
     //TODO: PRSGEN-2
 }
 
-
-
-
-
-std::vector<NonTerminal *> GrammarParser::eliminateImmediateLeftRecursion(NonTerminal * non_terminal)
+std::vector<NonTerminal*> GrammarParser::eliminateImmediateLeftRecursion(NonTerminal* non_terminal)
 {
-	//create new non terminal and new nonterminal `
-	NonTerminal * new_non_terminal = new NonTerminal(non_terminal->getName_());
-	NonTerminal * non_terminal_dash = new NonTerminal(non_terminal->getName_() + "`");
+    //create new non terminal and new nonterminal `
+    NonTerminal* new_non_terminal = new NonTerminal(non_terminal->getName_());
+    NonTerminal* non_terminal_dash = new NonTerminal(non_terminal->getName_()+"`");
 
-	//create 2 lists of betas and alpahas to store the proper values extracted from the production rules
-	std::vector<std::vector<std::string>> betas;
-	std::vector<std::vector<std::string>> alphas;
-	//get the prduction rules and extract alphas and betas from it
-		std::vector<std::vector<std::string>> production_rules = non_terminal->getProduction_rules_();
+    //create 2 lists of betas and alpahas to store the proper values extracted from the production rules
+    std::vector<std::vector<std::string>> betas;
+    std::vector<std::vector<std::string>> alphas;
+    //get the prduction rules and extract alphas and betas from it
+    std::vector<std::vector<std::string>> production_rules = non_terminal->getProduction_rules_();
 
-		//iterate through the production rules  
-		std::vector< std::vector<std::string> >::iterator row;
-		std::vector<std::string>::iterator col;
-		for (row = production_rules.begin(); row != production_rules.end(); row++) {
-			std::vector<std::string> tmp_alpha;
-			std::vector<std::string> tmp_beta;
-			//check if nonterminal name equals  to first letter or not  to determine will it be added to alpha or beta vector
-			col = row->begin();
-			if (non_terminal->getName_().compare(*col) == 0)
-			{
-				// it's alpha then push the next of this non terminal to alpha vector
-				for (col = col +1; col != row->end(); col++) {
+    //iterate through the production rules
+    std::vector<std::vector<std::string> >::iterator row;
+    std::vector<std::string>::iterator col;
+    for (row = production_rules.begin(); row!=production_rules.end(); row++) {
+        std::vector<std::string> tmp_alpha;
+        std::vector<std::string> tmp_beta;
+        //check if nonterminal name equals  to first letter or not  to determine will it be added to alpha or beta vector
+        col = row->begin();
+        if (non_terminal->getName_().compare(*col)==0) {
+            // it's alpha then push the next of this non terminal to alpha vector
+            for (col = col+1; col!=row->end(); col++) {
 
-					tmp_alpha.push_back(*col);
-					
-				}
-				//append  nonterminal` as the given rule in the end of the alpha
-				tmp_alpha.push_back(non_terminal->getName_() + "`");
-				alphas.push_back(tmp_alpha);
-			}
-			else {
-				//same for beta but it will work from the start of the string
-				for (col = row->begin(); col != row->end(); col++) {
+                tmp_alpha.push_back(*col);
 
-					tmp_beta.push_back(*col);
+            }
+            //append  nonterminal` as the given rule in the end of the alpha
+            tmp_alpha.push_back(non_terminal->getName_()+"`");
+            alphas.push_back(tmp_alpha);
+        }
+        else {
+            //same for beta but it will work from the start of the string
+            for (col = row->begin(); col!=row->end(); col++) {
 
-				}
-				tmp_beta.push_back(non_terminal->getName_() + "`");
-				betas.push_back(tmp_beta);
-			}
+                tmp_beta.push_back(*col);
 
+            }
+            tmp_beta.push_back(non_terminal->getName_()+"`");
+            betas.push_back(tmp_beta);
+        }
 
-		}
-		//addinng epsilon to nonterminal` 
-		std::vector<std::string> epsilon;
-		epsilon.push_back( "\\L");
-		alphas.push_back(epsilon);
+    }
+    //addinng epsilon to nonterminal`
+    std::vector<std::string> epsilon;
+    epsilon.push_back("\\L");
+    alphas.push_back(epsilon);
 
+    new_non_terminal->setProduction_rules_(betas);
+    non_terminal_dash->setProduction_rules_(alphas);
 
+    std::vector<NonTerminal*> returned_terminals;
+    returned_terminals.push_back(new_non_terminal);
+    returned_terminals.push_back(non_terminal_dash);
 
-		new_non_terminal->setProduction_rules_(betas);
-		non_terminal_dash->setProduction_rules_(alphas);
-
-		std::vector<NonTerminal *> returned_terminals;
-		returned_terminals.push_back(new_non_terminal);
-		returned_terminals.push_back(non_terminal_dash);
-
-	return  returned_terminals;
+    return returned_terminals;
 }
 
-
-bool GrammarParser::checkImmediateLeftRecursion(NonTerminal * non_terminal)
+bool GrammarParser::checkImmediateLeftRecursion(NonTerminal* non_terminal)
 {
-	//get the production rules of the non terminal
-	std::vector<std::vector<std::string>> production_rules = non_terminal->getProduction_rules_();
+    //get the production rules of the non terminal
+    std::vector<std::vector<std::string>> production_rules = non_terminal->getProduction_rules_();
 
-	//iterate through the production rules  
-	std::vector< std::vector<std::string> >::iterator row;
-	std::vector<std::string>::iterator col;
-	for (row = production_rules.begin(); row != production_rules.end(); row++) {
-		col = row->begin();
-		//check if the first nonterminal symbol is the same as the name of nonterminal
-		if (non_terminal->getName_().compare(*col) == 0)
-		{
-			return true;
+    //iterate through the production rules
+    std::vector<std::vector<std::string> >::iterator row;
+    std::vector<std::string>::iterator col;
+    for (row = production_rules.begin(); row!=production_rules.end(); row++) {
+        col = row->begin();
+        //check if the first nonterminal symbol is the same as the name of nonterminal
+        if (non_terminal->getName_().compare(*col)==0) {
+            return true;
 
-		}	
-	}
+        }
+    }
 
-	return false;
+    return false;
 }
 
-NonTerminal * GrammarParser::subsitution(NonTerminal * current_non_terminal, NonTerminal * input)
+NonTerminal* GrammarParser::subsitution(NonTerminal* current_non_terminal, NonTerminal* input)
 {
 
-	std::vector<std::vector<std::string>> production_rules_current = current_non_terminal->getProduction_rules_();
-	std::vector<std::vector<std::string>> production_rules_input = input->getProduction_rules_();
+    std::vector<std::vector<std::string>> production_rules_current = current_non_terminal->getProduction_rules_();
+    std::vector<std::vector<std::string>> production_rules_input = input->getProduction_rules_();
+
+    std::vector<std::vector<std::string> >::iterator row;
+    std::vector<std::string>::iterator col;
+
+    std::vector<std::vector<std::string> > tmp_prod_rules;
+    std::vector<std::string> tmp_vstr;
+
+    //loop through current non terminal elements
+    for (row = production_rules_current.begin(); row!=production_rules_current.end(); row++) {
+
+        col = row->begin();
+        // check if first letter is identical to the input name
+        if (input->getName_().compare(*col)!=0) {
+            //if not identical copy all vectors to new production rule as there wont be any change in them
+            for (col = row->begin(); col!=row->end(); col++) {
+
+                tmp_vstr.push_back(*col);
+
+            }
+            tmp_prod_rules.push_back(tmp_vstr);
+            tmp_vstr.clear();
+        }
+        else {
+            //if identical clone the current vector into the new production rule times the input length
 
 
-	std::vector< std::vector<std::string> >::iterator row;
-	std::vector<std::string>::iterator col;
+            std::vector<std::vector<std::string> >::iterator input_row;
+            std::vector<std::string>::iterator input_col;
 
-	std::vector< std::vector<std::string> > tmp_prod_rules;
-	std::vector<std::string> tmp_vstr;
-
-	//loop through current non terminal elements
-	for (row = production_rules_current.begin(); row != production_rules_current.end(); row++)
-	{
-		
-			col = row->begin();
-			// check if first letter is identical to the input name
-			if (input->getName_().compare(*col) != 0)
-			{
-				//if not identical copy all vectors to new production rule as there wont be any change in them 
-				for (col = row->begin(); col != row->end(); col++) 
-				{
-
-					tmp_vstr.push_back(*col);
-
-
-
-
-				}
-				tmp_prod_rules.push_back(tmp_vstr);
-				tmp_vstr.clear();
-			}
-			else
-
-			{
-				//if identical clone the current vector into the new production rule times the input length 
-
-
-				std::vector< std::vector<std::string> >::iterator input_row;
-				std::vector<std::string>::iterator input_col;
-				
 //					
 
-				for (input_row = production_rules_input.begin(); input_row != production_rules_input.end(); input_row++) {
-					
-				//concat input production rules with one I want to substitue with from the beginning 
-				std::vector<std::string> the_copy (*input_row);
-				//skip the substituted symbol and continue concat with the given non terminal
-				for (col = row->begin()+1; col != row->end(); col++)
-				{
-					the_copy.push_back(*col);
-				}
-				tmp_prod_rules.push_back(the_copy);
-				}
-			}
+            for (input_row = production_rules_input.begin(); input_row!=production_rules_input.end(); input_row++) {
 
-			
-	}
+                //concat input production rules with one I want to substitue with from the beginning
+                std::vector<std::string> the_copy(*input_row);
+                //skip the substituted symbol and continue concat with the given non terminal
+                for (col = row->begin()+1; col!=row->end(); col++) {
+                    the_copy.push_back(*col);
+                }
+                tmp_prod_rules.push_back(the_copy);
+            }
+        }
 
-	NonTerminal * new_non_terminal = new NonTerminal(current_non_terminal->getName_());
-	new_non_terminal->setProduction_rules_(tmp_prod_rules);
+    }
 
+    NonTerminal* new_non_terminal = new NonTerminal(current_non_terminal->getName_());
+    new_non_terminal->setProduction_rules_(tmp_prod_rules);
 
-	return new_non_terminal;
+    return new_non_terminal;
 }
-
-
 
 void GrammarParser::eliminateLeftFactoring()
 {
     //TODO: PRSGEN-4
 }
 
-template <typename T>
+template<typename T>
 std::vector<T*> convertFrom(std::vector<T>& source)
 {
-	std::vector<T*> target(source.size());
-	std::transform(source.begin(), source.end(), target.begin(), [](T& t) { return &t; });
-	return target;
+    std::vector<T*> target(source.size());
+    std::transform(source.begin(), source.end(), target.begin(), [](T& t) { return &t; });
+    return target;
 }
-
-
 
 void GrammarParser::eliminateLeftRecursion(std::vector<NonTerminal> non_terminal_list)
 {
-	std::vector<NonTerminal *> non_terminal_list_p = convertFrom (non_terminal_list);
-	
-	
+    std::vector<NonTerminal*> non_terminal_list_p = convertFrom(non_terminal_list);
 
-	std::vector<NonTerminal*> new_non_terminal_list;
-	std::vector<NonTerminal*> ::iterator non_terminal_iterator = non_terminal_list_p.begin();
-	
-	for (int i = 0; i < non_terminal_list_p.size(); i = i++) 
-	{
-		NonTerminal* processing_non_terminal = *(non_terminal_list_p.begin()+i);
-		for (int j = 0; j < i; j++) 
-		{
-			processing_non_terminal = subsitution(processing_non_terminal , *(non_terminal_iterator + j)  );
-		
-		}
-		if (checkImmediateLeftRecursion(processing_non_terminal))
-		{
-			std::vector<NonTerminal*> after_immediate_fix = eliminateImmediateLeftRecursion(processing_non_terminal);
-			new_non_terminal_list.insert(new_non_terminal_list.end(), after_immediate_fix.begin(), after_immediate_fix.end());
-			*(non_terminal_list_p.begin() + i) = after_immediate_fix[0];
-		}
-		else
-		{
-			new_non_terminal_list.push_back(*non_terminal_list_p.begin() + i);
-		}
-	}
-	non_terminals_.clear();
-	non_terminals_.reserve(non_terminal_list_p.size());
+    std::vector<NonTerminal*> new_non_terminal_list;
+    std::vector<NonTerminal*>::iterator non_terminal_iterator = non_terminal_list_p.begin();
 
-for (int j = 0; j < new_non_terminal_list.size(); j++)
-{
-	non_terminals_.push_back( **(new_non_terminal_list.begin() + j ));
+    for (int i = 0; i<non_terminal_list_p.size(); i++) {
+        NonTerminal* processing_non_terminal = *(non_terminal_list_p.begin()+i);
+        for (int j = 0; j<i; j++) {
+            processing_non_terminal = subsitution(processing_non_terminal, *(non_terminal_iterator+j));
 
+        }
+        if (checkImmediateLeftRecursion(processing_non_terminal)) {
+            std::vector<NonTerminal*> after_immediate_fix = eliminateImmediateLeftRecursion(processing_non_terminal);
+            new_non_terminal_list.insert(new_non_terminal_list.end(), after_immediate_fix.begin(),
+                    after_immediate_fix.end());
+            *(non_terminal_list_p.begin()+i) = after_immediate_fix[0];
+        }
+        else {
+            new_non_terminal_list.push_back(*non_terminal_list_p.begin()+i);
+        }
+    }
+    non_terminals_.clear();
+    non_terminals_.reserve(non_terminal_list_p.size());
 
-}
-	
+    for (int j = 0; j<new_non_terminal_list.size(); j++) {
+        non_terminals_.push_back(**(new_non_terminal_list.begin()+j));
+
+    }
+
 }
